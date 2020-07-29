cc20a5293d64690cee9ef11b6541dc6af2e217c7
Mark Butler <mrkcbutler@gmail.com>
2020-07-29 09:29:46 -0500

    Define function to scrape cleanup reports

--- Data_scraping/scrape.py
+++ Data_scraping/scrape.py
@@ -16,6 +16,9 @@ import time
 from bs4 import BeautifulSoup
 import requests
 
+# _BASE_URL is prepended to relative urls in following links.
+_BASE_URL = 'https://www.rrc.state.tx.us'
+
 # Delay between requests for data from RRC.  Browsing the site shows that the
 # it can be slow, so the delay is set to be long.
 _DELAY = 5
@@ -36,9 +39,12 @@ _DATA_ROOT = 'data'
 # saved html is used to develop detailed scraping commands.
 _SAVED_PAGES = 'saved_pages'
 
+########################################
+# Functions that get fetch/save data
+########################################
 
 def _get(url):
-    """Perform repetitive tasks involved executing HTTP GET."""
+    """Perform repetitive tasks for executing HTTP GET."""
 
     headers = {'user-agent': _USER_AGENT}
     r = requests.get(url, headers = headers)
@@ -93,6 +99,32 @@ def get_soup(url, filename):
 
     return soup
 
+########################################
+# URL-specific scraping functions
+########################################
+
+# Unlike the html available from the browser as the page source, the html
+# obtained using the requests module has few attributes that can be used to
+# select individual tags.  Here is a typical example:
+#
+# <h3>
+#  <a id="OCP_quarterly">
+#  </a>
+#  Oilfield Cleanup Program Quarterly Reports
+# </h3>
+# <table height="44" width="752">
+#  <tbody>
+#   <tr>
+#    <td>
+#     <strong>
+#      FY 2020
+#     </strong>
+#    </td>
+#    ...
+#
+# So in locating relevant tags, a natural approach is to start from one of the
+# few tags with an id attribute and then navigate to the tag(s) to be scraped.
+# This approach is used in the functions below.
 
 def get_cleanup_reports():
     """Scrape reports on efforts to plug and clean up abandoned wells."""
@@ -100,6 +132,58 @@ def get_cleanup_reports():
     url = 'https://www.rrc.state.tx.us/oil-gas/environmental-cleanup-programs/oil-gas-regulation-and-cleanup-fund/'
     soup = get_soup(url, 'cleanup_reports.html')
 
+    header = soup.find(id = 'OCP_quarterly').parent
+    _get_quarterly_reports(header)
+
+    header = soup.find(id = 'OCP_annual').parent
+    _get_annual_reports(header)
+
+# The helper functions _get_quarterly reports and _get_annual_reports could be
+# defined as inner functions of get_cleanup_reports, but they are instead
+# defined at internal module functions in order to keep all functions relatively
+# short and readable.
+
+def _get_quarterly_reports(header):
+    table = header.find_next_sibling()
+    rows = table.find_all('tr')
+
+    # Create a list of subdirectories in which the quarterly reports for an
+    # individual year will be stored.
+    year_tags = rows[0].find_all(_has_string)
+    years = [year_tag.string.strip().replace(' ', '_')
+             for year_tag in year_tags]
+    quarterly_reports_dir = Path('Cleanup_reports') / 'Quarterly'
+    year_dirs = [quarterly_reports_dir / year
+                 for year in years]
+
+    # Download and save the quarterly reports.  The filenames are inconsistent
+    # for different years.  The text of the a tags that link to the reports is
+    # used to generate consistent, understandable filenames.
+    for row in rows[1:]:
+        cells = row.find_all('td')
+        for index, cell in enumerate(cells):
+            a_tag = cell.find('a')
+            if a_tag:
+                filename = a_tag.string.strip().replace(' ', '_') + '.pdf'
+                relative_path = year_dirs[index] / filename
+                url = _BASE_URL + a_tag['href']
+                get_binary_file(url, relative_path)
+
+
+def _get_annual_reports(header):
+    table = header.find_next_sibling()
+    annual_reports_dir = Path('Cleanup_reports') / 'Annual'
+    a_tags = table.find_all('a')
+    for a_tag in a_tags:
+        filename = a_tag.string.strip().replace(' ', '_') + '.pdf'
+        relative_path = annual_reports_dir / filename
+        url = _BASE_URL + a_tag['href']
+        get_binary_file(url, relative_path)
+
+
+def _has_string(tag):
+    """Search function used with find_all in BeautifulSoup."""
+    return tag.string
 
 def get_distribution_reports():
     """Scrape reports giving the distribution of wells (including abandoned wells)."""
@@ -123,6 +207,9 @@ def get_abandoned_wells_report():
     url = 'https://www.rrc.state.tx.us/oil-gas/research-and-statistics/well-information/orphan-wells-12-months/'
     soup = get_soup(url, 'abandoned_wells.html')
 
+########################################
+# Main function
+########################################
 
 def main():
     """Execute the functions defined in this module to collect data on abandoned
