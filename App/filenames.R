deb93775bf1c64c170dea9b312ae821d584a7c0d
Mark Butler <mrkcbutler@gmail.com>
2020-08-13 13:10:22 -0500

    Add filenames.R
    This file gives a list of the filenames used in the R web app, avoiding
    hard-coded filenames scattered throughout the R code.

---
+++ App/filenames.R
@@ -0,0 +1,5 @@
+# Filenames/paths used by the app.
+abandoned_wells_csv_path <- './data/abandoned_wells.csv'
+counties_json_path <- './data/counties.geojson'
+counties_csv_path <- './data/counties.csv'
+districts_csv_path <- './data/RRC_district_codes.csv'
