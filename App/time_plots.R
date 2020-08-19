3057c8d2c6679800994a41c4a94626b448fdaaee
Mark Butler <mrkcbutler@gmail.com>
2020-08-19 08:19:44 -0500

    Add plots showing categories of unplugged wells

--- App/time_plots.R
+++ App/time_plots.R
@@ -1,14 +1,19 @@
+library(dplyr)
 library(ggplot2)
 library(scales)
 library(tidyr)
 
-# Functions in this file generate plots that show how the status of abandoned
-# wells in Texas has varied over time.
+# Functions in this file generate plots that show how the variation over time
+# of different categories of wells in Texas.
 
 source('filenames.R')
 
-unplugged_wells <- read.csv(unplugged_wells_path)
 plugging_history <- read.csv(plugging_history_path)
+unplugged_wells <- read.csv(unplugged_wells_path, colClasses = c(Date = 'Date')) %>%
+    select(Date, Active_wells, Inactive_under_1_yr, Inactive_under_1_yr_delinq_P5) %>%
+    rename(Active = Active_wells,
+           Recently_inactive = Inactive_under_1_yr,
+           Recently_abandoned = Inactive_under_1_yr_delinq_P5)
 
 plot_plugging_history <- function() {
     # To simplify the plotting process with ggplot, gather the values of two
@@ -29,6 +34,20 @@ plot_plugging_history <- function() {
                            labels = label_comma()) +
         scale_x_continuous(name = 'Year') +
         ggtitle('Progress in plugging abandoned wells') +
+        theme_grey(base_size = 18)
+
+    return(fig)
+}
+
+plot_distribution_history <- function(column_name, title) {
+
+    fig <- ggplot(unplugged_wells,
+                  aes_string(x = 'Date', y = column_name)) +
+        geom_point(color = 'navyblue') +
+        scale_y_continuous(name = 'Number of wells',
+                           limits = c(0, NA),
+                           labels = label_comma()) +
+        ggtitle(title) +
         theme_grey(base_size = 18)
 
     return(fig)
