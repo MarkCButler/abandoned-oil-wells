eaf81a68f7b62df3a5daff0d2ec63dd51fa4d1f6
Mark Butler <mrkcbutler@gmail.com>
2020-08-20 18:31:21 -0500

    Define reactive elements in server.R

--- App/server.R
+++ App/server.R
@@ -1,26 +1,64 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(plotly)
 library(shiny)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+source('status_plots.R')
+source('time_plots.R')
 
-    output$distPlot <- renderPlot({
+server <- function(input, output) {
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    output$county_totals_map <- renderPlotly({
+        generate_map(data = region_totals, data_label = 'Counties')
     })
 
-})
+    output$county_totals_hist <- renderPlot({
+        plot_county_totals()
+    })
+
+    output$district_totals_map <- renderPlotly({
+        generate_map(data = region_totals, data_label = 'Districts')
+    })
+
+    output$district_totals_hist <- renderPlot({
+        plot_district_totals()
+    })
+
+    output$inactive_period_texas <- renderPlot({
+        plot_inactive_period()
+    })
+
+    output$county_totals_old_wells <- renderPlotly({
+        generate_map(data = region_totals_old_wells,
+                     data_label = 'Counties',
+                     months_inactive = 240)
+    })
+
+    output$inactive_period_pecos <- renderPlot({
+        plot_inactive_period(county_name = 'Pecos')
+    })
+
+    output$inactive_period_hutchinson <- renderPlot({
+        plot_inactive_period(county_name = 'Hutchinson')
+    })
+
+    output$plugging_history <- renderPlot({
+        plot_plugging_history()
+    })
+
+    output$active_wells <- renderPlot({
+        plot_distribution_history(column_name = 'Active', title = 'Active wells in Texas')
+    })
+
+    output$recently_inactive <- renderPlot({
+        plot_distribution_history(column_name = 'Recently_inactive',
+                                  title = 'Wells inactive < 1 year')
+    })
+
+    output$recently_abandoned <- renderPlot({
+        plot_distribution_history(column_name = 'Recently_abandoned',
+                                  title = 'Wells inactive < 1 year, delinquent on paperwork')
+    })
+
+    output$stock_price <- renderPlot({
+        plot_price_history()
+    })
+}
