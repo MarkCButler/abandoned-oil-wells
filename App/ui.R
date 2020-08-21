6afa0efd435c280c451d0665db04e96bff9b5dff
Mark Butler <mrkcbutler@gmail.com>
2020-08-21 08:59:47 -0500

    Add initial draft of user interface
    All plots are included in the interface, but there are no headings or
    text.

--- App/ui.R
+++ App/ui.R
@@ -1,33 +1,130 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(plotly)
 library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+############################
+# Define header and sidebar
+############################
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+header <- dashboardHeader(title = 'Abandoned Oil and Gas Wells in Texas',
+                          titleWidth = 400)
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem('Summary', tabName = 'summary', icon = icon('hand-point-right'), selected = T)
+    )
+)
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+###############################################################
+# Define the layout of the five tabItems in the dashboard body
+###############################################################
+
+data_source_wells <- 'https://www.rrc.state.tx.us/'
+data_source_stocks <- 'https://finance.yahoo.com/'
+source_code <- 'https://github.com/MarkCButler/abandoned-wells-app'
+summary_tab <- tabItem(
+    tabName = 'summary',
+    fluidRow(
+        column(
+            width = 10,
+            plotlyOutput('county_totals_map')
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('county_totals_hist'),
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotlyOutput('district_totals_map')
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('district_totals_hist'),
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotlyOutput('county_totals_old_wells')
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('inactive_period_texas'),
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('inactive_period_pecos'),
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('inactive_period_hutchinson'),
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('plugging_history'),
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('active_wells'),
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('recently_inactive'),
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('recently_abandoned'),
+        )
+    ),
+    fluidRow(
+        column(
+            width = 10,
+            plotOutput('stock_price'),
+        )
+    ),
+    br(),
+    fluidRow(
+        column(
+            width = 12,
+            h4('Data sources'),
+            p('Oil and gas wells: ',
+              a(href = data_source_wells, data_source_wells)),
+            p('Stock prices:  ',
+              a(href = data_source_stocks, data_source_stocks)),
+            h4('source_code'),
+            a(href = source_code, source_code)
         )
     )
-))
+)
+
+#####################################################
+# Define the dashboard body and create the dashboard
+#####################################################
+
+body <- dashboardBody(
+    tabItems(
+        summary_tab
+    )
+)
+
+dashboardPage(header, sidebar, body)
